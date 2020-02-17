4148cb6d08d8620ff78281ef91b5927673d63c50
Razvan Marinovici <razvan.marinovici@codefysoftware.com>
2020-02-17 09:53:11 +0200

    Remove code not used

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 export * from './src/activation.execution';
 export * from './src/activation.generator';
-export * from './src/activation.result';
 export * from './src/attributes/singleton';
 export * from './src/attributes/use.activation';
 export * from './src/context';
